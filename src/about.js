26fc2c637a6d6260c84fc9c2bec0e419e0bc0c9f
marin0suke <monk.marina@gmail.com>
2024-11-02 19:21:21 +1000

    Add tabbed browsing functionality

--- src/about.js
+++ src/about.js
@@ -2,8 +2,8 @@ export const about = () => {
     const container = document.createElement("div");
     
     const aboutHeader = document.createElement("h1");
-    name.classList.add("about-header");
-    name.textContent = "Proudly caffienating our locals, tourists and hardworking crew since 2029 as the first diner establishment on Mars!";
+    aboutHeader.classList.add("about-header");
+    aboutHeader.textContent = "Proudly caffienating our locals, tourists and hardworking crew since 2029 as the first diner establishment on Mars!";
     container.appendChild(aboutHeader);
 
     return container;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import { homepage } from "./homepage.js";
 import { menu } from "./menu.js"
-
+import { about } from "./about.js"
 
 const content = document.querySelector(".content");
-content.appendChild(homepage());
 
-const menu = document.querySelector(".menu");
-content.appendChild(menu());
 
-const about = document.querySelector(".about");
-content.appendChild(about());
+const homeButton = document.querySelector(".home-content");
+homeButton.addEventListener("click", () => {
+    content.textContent = ""; // textContent is enough when rendering static and simple content. otherwise can use firstChild conditional to remove.
+    content.appendChild(homepage());
+});
+
+const menuButton = document.querySelector(".menu-content");
+menuButton.addEventListener("click", () => {
+    content.textContent = ""; // textContent is enough when rendering static and simple content. otherwise can use firstChild conditional to remove.
+    content.appendChild(menu());
+});
+
+const aboutButton = document.querySelector(".about-content");
+aboutButton.addEventListener("click", () => {
+    content.textContent = ""; // textContent is enough when rendering static and simple content. otherwise can use firstChild conditional to remove.
+    content.appendChild(about());
+});
+
 
 // best practice here? on button click - need to clear content div, then show?
 // default will be home page when page is loaded.
